dc1f10f0b6792669c131165d0794bac27b0c424f
Daniel Navarro <luis.litt117@gmail.com>
2021-07-07 22:32:56 -0700

    Ajustes en rutas

--- routes/movie-info.py
+++ routes/movie-info.py
@@ -5,40 +5,46 @@ from modules.cors import enable_cors
 import modules.utils as utils
 from modules.auth import auth_required
 
-
-## Routes
+app = bottle.Bottle()
 
 ## Add a movie
 @app.post("/movie-info/add")
 def add_a_movie(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
 
 ## Get movies list
 @app.get("/movie-info/list")
 def get_movies_list(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
 
 ## Get movie details
 @app.get("/movie-info/<movie_id>")
 def get_movie_details(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
 
 ## Update movie details
 @app.post("/movie-info/<movie_id>")
 def update_movie_details(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
 
 ## Get all reviews from a movie
 @app.get("/movie-info/<movie_id>/review")
 def get_all_movie_reviews(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
 
 ## Add a review to a certain movie
 @app.post("/movie-info/<movie_id>/review")
 def add_a_review(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
 
 ## Get a certain review from a certain movie
 @app.get("/movie-info/<movie_id>/review/<review_id>")
 def get_review_from_movie(*args, **kwargs):
+    bottle.response.content_type = "application/json"
     return dict(code=501, "Not implemented")
